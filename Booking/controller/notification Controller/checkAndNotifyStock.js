c7530474e87ee5d5424eb661d867dbdb50786a91
devMitulT <manntari2004@gmail.com>
2026-02-11 15:28:27 +0530

    fix : add stock order filled in notification

--- Booking/controller/notification Controller/checkAndNotifyStock.js
+++ Booking/controller/notification Controller/checkAndNotifyStock.js
@@ -1,38 +1,44 @@
-// utils/stockNotifier.js
-
 const Notification = require("../../models/notificationModel");
 
+
 const checkAndNotifyStock = async (product, session = null) => {
-  const { stock, sku, thresholdStock, _id, organizationId, name } = product;
+ const { stock, sku, thresholdStock, _id, organizationId, name } = product;
 
-  const isCritical = stock < 0.2 * thresholdStock;
-  const isLow = stock < thresholdStock;
 
-  if (isCritical) {
-    await Notification.create(
-      [
-        {
-          type: "danger",
-          message: `Stock of $${sku} : ${name} is critically low!`,
-          product: _id,
-          organizationId,
-        },
-      ],
-      session ? { session } : {}
-    );
-  } else if (isLow) {
-    await Notification.create(
-      [
-        {
-          type: "warning",
-          message: `Stock of $${sku} : ${name} is below the threshold.`,
-          product: _id,
-          organizationId,
-        },
-      ],
-      session ? { session } : {}
-    );
-  }
+ const isCritical = stock < 0.2 * thresholdStock;
+ const isLow = stock < thresholdStock;
+
+
+ if (isCritical) {
+   await Notification.create(
+     [
+       {
+         type: "danger",
+         message: `Stock of $${sku} : ${name} is critically low!`,
+         product: _id,
+         organizationId,
+         stockAtOrder :stock
+       },
+     ],
+     session ? { session } : {}
+   );
+ } else if (isLow) {
+   await Notification.create(
+     [
+       {
+         type: "warning",
+         message: `Stock of $${sku} : ${name} is below the threshold.`,
+         product: _id,
+         organizationId,
+         stockAtOrder :stock
+       },
+     ],
+     session ? { session } : {}
+   );
+ }
 };
 
+
 module.exports = checkAndNotifyStock;
+
+
